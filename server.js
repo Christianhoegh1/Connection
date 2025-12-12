9e6ec0a88cbff3ad3583ef253bf4281a66b12731
Christianhoegh1 <149064106+Christianhoegh1@users.noreply.github.com>
2025-12-12 17:37:10 +0700

    Update server.js

---
+++ server.js
@@ -0,0 +1,24 @@
+const WebSocket = require("ws");
+
+const PORT = process.env.PORT || 8080;
+const wss = new WebSocket.Server({ port: PORT });
+
+let clients = [];
+
+wss.on("connection", ws => {
+  clients.push(ws);
+
+  ws.on("message", msg => {
+    clients.forEach(client => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(msg);
+      }
+    });
+  });
+
+  ws.on("close", () => {
+    clients = clients.filter(c => c !== ws);
+  });
+});
+
+console.log("Signaling server running on port " + PORT);
